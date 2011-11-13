9d300ef56a2073f6208347a41c3c7ee8c925763e
Luke Campbell <luke.s.campbell@gmail.com>
2011-11-13 16:59:39 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Memo::Application.config.secret_token = '256fa0983de933bb19d5f07d63c19d10859b82d8bfbf544a0257aad228a121d75133249dd3da06ca2444a66960416aee34d7b8315ff388070ff9bfe09f880859'
